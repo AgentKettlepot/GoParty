77cb7e723d4a0563e867318c8591ef072f2f4eef
Keith Yao <keithyao1012@gmail.com>
2023-03-04 07:34:56 -0500

    Wrote base code for server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-// Test commit
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+app.use(express.json())
+
+mongoose.connect(process.env.MONG_URI).then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log("Listening on Port 4000!!!")
+    })
+}).catch((error)=>{
+    console.log(error)
+})

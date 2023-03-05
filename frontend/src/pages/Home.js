ae974ade24b18347c61938eda98b485ff8616753
Keith Yao <keithyao1012@gmail.com>
2023-03-04 19:16:08 -0500

    Started data fetching

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+
 const Home= () =>{
+
+    const [parties, setParties] = useState(null)
+    useEffect(()=>{
+        const fetchParties= async()=>{
+            const response = await fetch('/goParty')
+            const json = await response.json()
+
+            if (response.ok){
+                setParties(json)
+            }
+        }
+
+        fetchParties()
+    }, [])
+
     return(
         <div className="home">
-            <h2>Home</h2>
+            <div classNames="parties">
+                {parties && parties.map((party)=>(
+                    <p key={party._id}>{party.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
